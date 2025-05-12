1652bd8adc2ce9f806d227b1bf8e7d21cc517e33
kowstav <kowstavbiswas@gmail.com>
2025-05-12 11:24:08 +0530

    V6.1

--- src/components/Widget.jsx
+++ src/components/Widget.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Widget.module.css";
 import Transcription from "./Transcription";
 import WidgetButtons from "./WidgetButtons";
 
 const Widget = () => {
+  const [isFullscreen, setIsFullscreen] = useState(false);
+
+  const handleFullscreen = () => setIsFullscreen(f => !f);
+
   return (
-    <div className={styles.widget}>
+    <div
+      className={`${styles.widget} ${isFullscreen ? styles.fullscreen : ""}`}
+    >
       <Transcription />
-      <WidgetButtons />
+      <WidgetButtons
+        onFullscreen={handleFullscreen}
+        isFullscreen={isFullscreen}
+      />
     </div>
   );
 };
